5fd6e0bedbcdd50e82019c1ee98a0911cc6757c9
Myrrh Khan <myrrhkhan@gmail.com>
2025-01-16 00:52:52 -0400

    multiple cameras

--- get_cameras.cpp
+++ get_cameras.cpp
@@ -5,18 +5,22 @@
 #include <stdio.h>
 
 int main(int, char **) {
-  cv::Mat frame;
+  cv::Mat cam_frame1;
+  cv::Mat cam_frame2;
   //--- INITIALIZE VIDEOCAPTURE
-  cv::VideoCapture cap;
+  cv::VideoCapture cap1;
+  cv::VideoCapture cap2;
   // open the default camera using default API
   // cap.open(0);
   // OR advance usage: select any API backend
-  int deviceID = 0;        // 0 = open default camera
+  int cam1ID = 1; // 0 = open default camera
+  int cam2ID = 2;
   int apiID = cv::CAP_ANY; // 0 = autodetect default API
   // open selected camera using selected API
-  cap.open(deviceID, apiID);
+  cap1.open(cam1ID, apiID);
+  cap2.open(cam2ID, apiID);
   // check if we succeeded
-  if (!cap.isOpened()) {
+  if (!cap1.isOpened() || !cap2.isOpened()) {
     std::cerr << "ERROR! Unable to open camera\n";
     return -1;
   }
@@ -25,14 +29,16 @@ int main(int, char **) {
             << "Press any key to terminate" << std::endl;
   for (;;) {
     // wait for a new frame from camera and store it into 'frame'
-    cap.read(frame);
+    cap1.read(cam_frame1);
+    cap2.read(cam_frame2);
     // check if we succeeded
-    if (frame.empty()) {
+    if (cam_frame1.empty() || cam_frame2.empty()) {
       std::cerr << "ERROR! blank frame grabbed\n";
       break;
     }
     // show live and wait for a key with timeout long enough to show images
-    imshow("Live", frame);
+    imshow("Camera 1", cam_frame1);
+    imshow("Camera 2", cam_frame2);
     if (cv::waitKey(5) >= 0)
       break;
   }
