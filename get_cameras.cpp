81f4c4d67986ca1d7b4e75835c22dbfb97818624
Myrrh Khan <myrrhkhan@gmail.com>
2025-01-27 20:12:53 -0400

    version 1, oop'd

--- get_cameras.cpp
+++ get_cameras.cpp
@@ -1,27 +1,26 @@
+#include "get_cameras.hpp"
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/core/cvstd_wrapper.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/stitching.hpp>
 #include <opencv2/videoio.hpp>
 #include <stdio.h>
 #include <unistd.h>
 #include <vector>
 
-cv::Ptr<cv::VideoCapture> preview_device(int device_ID) {
+void CamStitcher::preview_device(int device_ID) {
   cv::Mat frame;
   //--- INITIALIZE VIDEOCAPTURE
   cv::Ptr<cv::VideoCapture> cap = cv::makePtr<cv::VideoCapture>();
-  // open the default camera using default API
-  // cap.open(0);
-  // OR advance usage: select any API backend
-  int api_ID = cv::CAP_ANY; // 0 = autodetect default API
   // open selected camera using selected API
-  cap->open(device_ID, api_ID);
+  cap->open(device_ID, m_API_ID);
   // check if we succeeded
   if (!cap->isOpened()) {
     std::cerr << "ERROR! Unable to open camera\n";
-    return nullptr;
+    return;
   }
+
   //--- GRAB AND WRITE LOOP
   std::cout << "Start grabbing from ID: " << device_ID << std::endl
             << "Press any key to terminate" << std::endl;
@@ -31,7 +30,7 @@ cv::Ptr<cv::VideoCapture> preview_device(int device_ID) {
     // check if we succeeded
     if (frame.empty()) {
       std::cerr << "ERROR! blank frame grabbed\n";
-      return NULL;
+      return;
     }
     // show live and wait for a key with timeout long enough to show images
     namedWindow("Live", cv::WINDOW_NORMAL);
@@ -40,22 +39,14 @@ cv::Ptr<cv::VideoCapture> preview_device(int device_ID) {
     if (cv::waitKey(5) >= 0)
       break;
   }
-  // the camera will be deinitialized automatically in VideoCapture destructor
-  return cap;
+  m_caps.push_back(cap);
 }
 
-std::vector<cv::Ptr<cv::VideoCapture>> get_cameras() {
-  int index = 0;
-  std::vector<cv::Ptr<cv::VideoCapture>> caps;
+CamStitcher::CamStitcher() {
   for (int device_ID = 0; device_ID < 10; device_ID++) {
-    cv::VideoCapture *cap = preview_device(device_ID);
-    if (cap) {
-      caps.push_back(cap);
-      std::cout << index << std::endl;
-      index++;
-    }
+    preview_device(device_ID);
   }
-  for (int i = 0; i < index; i++) {
+  for (int i = 0; i < m_caps.size(); i++) {
     std::cout << i << std::endl;
   }
 
@@ -66,9 +57,62 @@ std::vector<cv::Ptr<cv::VideoCapture>> get_cameras() {
   std::cin >> device2_idx;
 
   std::vector<cv::Ptr<cv::VideoCapture>> final_caps;
-  final_caps.push_back(caps[device1_idx]);
-  final_caps.push_back(caps[device2_idx]);
+  final_caps.push_back(m_caps[device1_idx]);
+  final_caps.push_back(m_caps[device2_idx]);
+
+  m_caps = final_caps;
+
+  // initialize frames:
+  for (int i = 0; i < m_caps.size(); i++) {
+    cv::Mat frame;
+    m_frames.push_back(frame);
+  }
 
   // https://docs.opencv.org/4.x/d8/d19/tutorial_stitcher.html
-  return final_caps;
+}
+
+void CamStitcher::run() {
+  for (;;) {
+    // step 1: get frame
+    for (int i = 0; i < m_caps.size(); i++) {
+      m_caps[i]->read(m_frames[i]);
+      if (m_frames[i].empty()) {
+        std::cerr << "ERROR! blank frame grabbed\n";
+        return;
+      }
+      // for (;;) {
+      //   namedWindow("Live", cv::WINDOW_NORMAL);
+      //   imshow("Live", m_frames[i]);
+      //   // cv::resizeWindow("Live", 360, 240);
+      //
+      //   if (cv::waitKey(5) >= 0)
+      //     break;
+      // }
+    }
+    // step 2: stitch together
+    cv::Mat pano;
+    cv::Ptr<cv::Stitcher> stitcher =
+        cv::Stitcher::create(cv::Stitcher::PANORAMA); // TODO why Ptr?
+    cv::Stitcher::Status status = stitcher->stitch(m_frames, pano);
+    std::cerr << "frames, pano" << std::endl;
+
+    if (status != cv::Stitcher::OK) {
+      std::cout << "Can't stitch images, error code = " << int(status)
+                << std::endl;
+      imshow("Cam 1", m_frames[0]);
+      imshow("Cam 2", m_frames[1]);
+      if (cv::waitKey(5) >= 0)
+        continue;
+      break;
+    }
+    // step 3: show
+    imshow("Live", pano);
+    if (cv::waitKey(5) >= 0)
+      break;
+  }
+}
+
+int main() {
+  CamStitcher stitcher = CamStitcher();
+  stitcher.run();
 }
--- get_cameras.hpp
+++ get_cameras.hpp
@@ -2,4 +2,14 @@
 #include <opencv2/videoio.hpp>
 #include <vector>
 
-std::vector<cv::Ptr<cv::VideoCapture>> get_cameras();
+class CamStitcher {
+public:
+  CamStitcher();
+  void run();
+
+private:
+  void preview_device(int device_ID);
+  std::vector<cv::Ptr<cv::VideoCapture>> m_caps;
+  int m_API_ID = cv::CAP_ANY;
+  std::vector<cv::Mat> m_frames;
+};
